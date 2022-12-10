e5283f5d71b1a96236665330f8e677ef36f50c9b
Parker Mathis <parkerjmathis@gmail.com>
2022-12-10 13:37:24 -0800

    Removed Develop folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@
 
 const express = require("express");
 const path = require("path");
-const notesData = require("./Develop/db/db.json");
+const notesData = require("./db/db.json");
 
 const PORT = 3001;
 
@@ -23,9 +23,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // GET Route for the notes page
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/notes.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
 // Wildcard GET route to direct users to the homepage (index.html)
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/index.html")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 app.listen(PORT, () => console.log(`Your app is listening at http://localhost:${PORT}`));
