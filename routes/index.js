381999ee412c87cecc4896f43eda03d6bd9003ad
Parker Mathis <parkerjmathis@gmail.com>
2022-12-12 17:58:22 -0800

    Update PORT and notes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+// A modular router setup isn't necessary for this project since we only have a single router, but could be used to expand easily in the future
+
 const express = require("express");
 
 // Import modular router for /notes
@@ -5,6 +7,7 @@ const notesRouter = require("./notes");
 
 const app = express();
 
+// Routes used in the notes router will be prefixed with /notes
 app.use("/notes", notesRouter);
 
 module.exports = app;
